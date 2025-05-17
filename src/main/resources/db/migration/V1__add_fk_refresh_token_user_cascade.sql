38d0856b1af1eb33005c1eb39dba54f66e341bfa
Leo <leo@pricingpact.com>
2025-05-17 23:25:05 +0200

    feat(init): flyway is now controlling db migration

--- src/main/resources/db/migration/V1__add_fk_refresh_token_user_cascade.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE refresh_token
-    ADD CONSTRAINT fk_refresh_token_user
-        FOREIGN KEY (users_id) REFERENCES users (id)
-            ON DELETE CASCADE;
--- src/main/resources/db/migration/V2__add_on_delete_cascade_to_refresh_token.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE refresh_token
-    DROP CONSTRAINT fksa85a9lsg3ldy1nkmvs9o5ium;
-
-ALTER TABLE refresh_token
-    ADD CONSTRAINT fksa85a9lsg3ldy1nkmvs9o5ium
-        FOREIGN KEY (users_id)
-            REFERENCES users(id)
-            ON DELETE CASCADE;
