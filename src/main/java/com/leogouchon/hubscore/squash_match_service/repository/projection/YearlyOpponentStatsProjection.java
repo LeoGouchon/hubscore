242fe8570ddb91085b5237ab189b90dfc9d3f504
l.gouchon <leo.gouchon@gmail.com>
2026-02-26 22:22:15 +0100

    refactor(SquashStats): add opponent stats on year stats

---
+++ src/main/java/com/leogouchon/hubscore/squash_match_service/repository/projection/YearlyOpponentStatsProjection.java
@@ -0,0 +1,5 @@
+package com.leogouchon.hubscore.squash_match_service.repository.projection;
+
+public interface YearlyOpponentStatsProjection extends OpponentStatsProjection {
+    Integer getYear();
+}
