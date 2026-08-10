aea3b4de0f3418a19c48cdd5b0266808b8256c48
l.gouchon <leo.gouchon@gmail.com>
2026-08-10 13:56:00 +0200

    fix: remove isAdmin properties inside me endpoint. Replaced by role

--- src/main/java/com/leogouchon/hubscore/user_service/dto/MeResponseDTO.java
+++ src/main/java/com/leogouchon/hubscore/user_service/dto/MeResponseDTO.java
@@ -12,22 +12,12 @@ public class MeResponseDTO {
     private UUID id;
     private String email;
     private Players player;
-    private boolean isAdmin;
     private String role;
-
-    public MeResponseDTO(UUID id, String email, Players player, boolean isAdmin) {
-        this.id = id;
-        this.email = email;
-        this.player = player;
-        this.isAdmin = isAdmin;
-        this.role = isAdmin ? "ADMIN" : "USER";
-    }
 
     public MeResponseDTO(UUID id, String email, Players player, String role) {
         this.id = id;
         this.email = email;
         this.player = player;
         this.role = role;
-        this.isAdmin = "ADMIN".equals(role);
     }
 }
