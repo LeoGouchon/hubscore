4ed1aba13db80fa4257cc6f7248de13715cc28f8
l.gouchon <leo.gouchon@gmail.com>
2026-08-21 12:05:43 +0200

    test: add dedicated test configuration with `application-test.yml`

--- src/test/java/com/leogouchon/hubscore/HubScoreApplicationTests.java
+++ src/test/java/com/leogouchon/hubscore/HubScoreApplicationTests.java
@@ -2,10 +2,12 @@ package com.leogouchon.hubscore;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(properties = {
-        "spring.flyway.enabled=false",
+        "spring.config.location=classpath:/application-test.yml",
 })
+@ActiveProfiles("test")
 class HubscoreApplicationTests {
 
     @Test
